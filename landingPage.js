c0ac7da7bbeaed132f7a9ae8f077be245affb1db
Kim Jonas Cabugayan <114128501+Mozzie06@users.noreply.github.com>
2022-11-05 20:32:59 +0800

    Create landingPage.js

---
+++ landingPage.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav1")
+    .style.width = "70%";
+}
+
+function closeNav() {
+    document.getElementById("sidenav1")
+    .style.width = "0px";
+}
